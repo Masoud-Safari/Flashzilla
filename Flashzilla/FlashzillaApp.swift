9d5a99ebda73be2bc86ebcda839cd44dc918b8bd
Masoud Safari <mmsafari@gmail.com>
2024-03-29 14:49:12 -0700

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Masoud Safari on 2024-03-29.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

c121b4dceffe1f3599b49c76e4c958d710297c87
Masoud Safari <mmsafari@gmail.com>
2024-03-30 20:03:58 -0700

    Add card model and its view.

---
+++ Flashzilla/Card.swift
@@ -0,0 +1,15 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Masoud Safari on 2024-03-30.
+//
+
+import Foundation
+
+struct Card {
+    var prompt: String
+    var answer: String
+    
+    static let example = Card(prompt: "What is the capital of Japan?", answer: "Tokyo")
+}
